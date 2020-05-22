9a8ef61d0ec1f97bd299277440d8112d0a59f5bc
firasselafi <tngeek98@gmail.com>
2020-05-22 08:18:42 +0100

    Add files via upload

--- login.php
+++ login.php
@@ -1,10 +1,63 @@
 <?php
+
+
             $host = "localhost";
             $dbUsername = "root";
             $dbPassword = "0000";
             $dbName = "demo";
-            mysql_connect($host, $dbUsername, $password);
-            mysql_select_db($dbName);
+
+
+            
+        $con = mysqli_connect($host,$dbUsername,$dbPassword,$dbName);
+        $email=$_POST['email'];
+        $password=$_POST['password'];
+        
+       /* if(isset($_POST['email'])){
+            
+            $email=$_POST['email'];
+            $password=$_POST['password'];
+            
+            $sql="select * from loginform where user='".$email."'AND Pass='".$password."' limit 1";
+            
+            $result=mysql_query($sql);
+            
+            if(mysql_num_rows($result)==1){
+                echo " You Have Successfully Logged in";
+                exit();
+            }
+            else{
+                echo " You Have Entered Incorrect Password";
+                exit();
+            }
+                
+        }
+        // // //
+
+          /*  $con = mysqli_connect($host, $dbUsername, $password $dbName);
+          */
+            // Check connection
+            if ( mysqli_connect_errno() ) {
+            // If there is an error with the connection, stop the script and display the error.
+                exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+            }
+            
+            
+            if ( !isset($_POST['email'], $_POST['password']) ) {
+                // Could not get the data that should have been sent.
+                exit('Please fill both the username and password fields!');
+            }
+            
+            $result = mysqli_query($con, "select * from users where email='".$email."' AND password='".$password."'
+            limit 1");
+            
+            $row = mysqli_fetch_array($result);
+            
+            if($row["email"]==$email && $row["password"]==$password)
+            header( "refresh:3;url=/ProjectTest/welcome.html" );
+            else
+            header( "refresh:0;url=/ProjectTest/failed.html" );
+/*
+
             
     if(isset($_POST['email'])) {
         $email = $_POST['email'];
@@ -26,7 +79,7 @@
         }
     }
 
-
+*/
     
 
 ?>
